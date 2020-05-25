48ce62e5cd3b0330b8457664e095996e3778fba2
Tyler Vipond <tylervipond@Tylers-MacBook-Pro.local>
2020-05-24 22:45:26 -0400

    added message to grab furniture action

--- src/systems/grab_system.rs
+++ src/systems/grab_system.rs
@@ -1,5 +1,6 @@
-use crate::components::{Grabbing, WantsToGrab};
-use specs::{Entities, Join, System, WriteStorage};
+use crate::components::{Grabbing, Name, WantsToGrab};
+use crate::services::GameLog;
+use specs::{Entities, Entity, Join, ReadExpect, ReadStorage, System, WriteExpect, WriteStorage};
 
 pub struct GrabSystem {}
 
@@ -8,17 +9,31 @@ impl<'a> System<'a> for GrabSystem {
         Entities<'a>,
         WriteStorage<'a, WantsToGrab>,
         WriteStorage<'a, Grabbing>,
+        ReadExpect<'a, Entity>,
+        WriteExpect<'a, GameLog>,
+        ReadStorage<'a, Name>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (entities, mut wants_to_grabs, mut grabbings) = data;
+        let (entities, mut wants_to_grabs, mut grabbings, player_entity, mut log, names) = data;
         for (wants_to_grab, entity) in (&mut wants_to_grabs, &entities).join() {
             match wants_to_grab.thing.get_entity() {
                 Some(thing) => {
-                    grabbings.insert(entity, Grabbing { thing }).expect("couldn't add grabbing to entity");
+                    grabbings
+                        .insert(entity, Grabbing { thing })
+                        .expect("couldn't add grabbing to entity");
+                    if entity == *player_entity {
+                        let name = match names.get(thing) {
+                            Some(name) => name.name.to_owned(),
+                            None => "unknown".to_string(),
+                        };
+                        log.add(format!("You grab {}", name))
+                    }
                 }
                 None => {
-                    grabbings.remove(entity).expect("couldn't remove grabbing from entity");
+                    grabbings
+                        .remove(entity)
+                        .expect("couldn't remove grabbing from entity");
                 }
             };
         }
