1c2e02c41de10ece423e37bdfb4969da1b0f04b6
tylervipond <tylervipond@gmail.com>
2020-05-28 20:55:37 -0400

    added intro artwork

--- src/artwork.rs
+++ src/artwork.rs
@@ -1,2 +1,3 @@
 pub const INTRO_ARTWORK: &str = include_str!("../artwork/apprentice_title.txt");
 pub const DEATH_ARTWORK: &str = include_str!("../artwork/lantern.txt");
+pub const SCENARIO_ARTWORK: &str = include_str!("../artwork/oldman_intro.txt");
--- src/screens/screen_intro.rs
+++ src/screens/screen_intro.rs
@@ -1,6 +1,7 @@
 use super::constants::{SCREEN_PADDING, SCREEN_WIDTH};
-use crate::ui_components::ui_paragraph::UIParagraph;
-use rltk::Rltk;
+use crate::artwork::SCENARIO_ARTWORK;
+use crate::ui_components::{ui_paragraph::UIParagraph, ui_text_line::UITextLine};
+use rltk::{Rltk, BLACK, WHITE};
 
 pub struct ScreenIntro {}
 
@@ -11,6 +12,9 @@ impl ScreenIntro {
 
     pub fn draw(&self, ctx: &mut Rltk) {
         ctx.cls();
+        SCENARIO_ARTWORK.lines().enumerate().for_each(|(idx, line)| {
+            UITextLine::new(0, idx as i32 + 1, WHITE, BLACK, line).draw(ctx)
+        });
         let text = "\"The time has come, my adventuring days are over.The last adventure falls to you, my apprentice.
   It is said that within the halls of the ancient underground fortress below our feet lies the a treasure of great power,
   an ancient talisman. Legends say that this talisman protects against time. I suppose it is more than clear why an
